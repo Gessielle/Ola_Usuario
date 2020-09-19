87830ba1a2ab81f47b888fc4fed27c537c4311fd
Gessielle <gessielle-barossi@hotmail.com>
2020-09-18 21:56:46 -0300

    Olá Usuáro

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Olá_Usuário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Digite seu nome:");
+            nome = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nome}!");
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
